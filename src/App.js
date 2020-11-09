6cc93e6538703897aa9178d0847d014ecac5090e
Bernhard Esperester <bernhard@esperester.de>
2020-11-09 18:20:53 +0100

    connects to postgraphile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+const GET_ARTICLES = gql`
+{
+  allArticles{
+    nodes{
+      id,
+      name
+    }
+  }
+}
+`
 
 function App() {
+  const { data, loading, error } = useQuery(GET_ARTICLES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <h1>Articles</h1>
+      
+      {data && data.allArticles && data.allArticles.nodes.map((article, index) => (
+        <div key={index}>
+          {article.name}
+        </div>
+      ))}
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Apollo GraphQL
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:5000/graphql'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
