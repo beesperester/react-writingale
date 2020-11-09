b01aa4ce28d179a77cac7ec46cd4e95b34ff0c4f
Bernhard Esperester <bernhard@esperester.de>
2020-11-09 21:38:02 +0100

    implements creating and updating of articles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React from 'react';
 
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { Mutation, Query } from 'react-apollo';
 import gql from "graphql-tag";
 
 const GET_ARTICLES = gql`
@@ -13,10 +14,180 @@ const GET_ARTICLES = gql`
     }
   }
 }
-`
+`;
 
-function App() {
+const CREATE_ARTICLE = gql`
+mutation CreateArticle($input: CreateArticleInput!) {
+  createArticle(input: $input) {
+    article {
+      id,
+      name
+    }
+  }
+}
+`;
+
+const UPDATE_ARTICLE = gql`
+mutation UpdateArticle($input: UpdateArticleByIdInput!) {
+  updateArticleById(input: $input) {
+    article {
+      id, name
+    }
+  }
+}
+`;
+
+const CreateArticle = () => {
+  let input;
+
+  return (
+    <Mutation 
+      mutation={CREATE_ARTICLE}
+      update={(cache, {data}) => {
+        const { allArticles } = cache.readQuery({
+          query: GET_ARTICLES
+        });
+
+        cache.writeQuery({
+          query: GET_ARTICLES,
+          data: {
+            allArticles: {
+              nodes: [
+                ...allArticles.nodes,
+                data.createArticle.article
+              ]
+            }
+          }
+        })
+      }}
+    >
+      {createArticle => (
+        <div>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+
+              if (!input.value) return;
+
+              createArticle({
+                variables: {
+                  input: {
+                    article: {
+                      name: input.value
+                    }
+                  }
+                }
+              });
+              input.value = '';
+            }}
+          >
+            <input
+              ref={node => {
+                input = node;
+              }}
+            />
+
+            <button type="submit">Create</button>
+
+          </form>
+        </div>
+      )}
+    </Mutation>
+  )
+}
+
+const UpdateArticle = ({id, name}) => {
+  let input;
+
+  return (
+    <Mutation
+      mutation={UPDATE_ARTICLE}
+    >
+      {updateArticle => (
+        <div>
+          <p>{name}</p>
+
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+
+              if (!input.value) return;
+
+              updateArticle({
+                variables: {
+                  input: {
+                    id: id,
+                    articlePatch: {
+                      name: input.value
+                    }
+                  }
+                }
+              });
+
+              input.value = '';
+            }}
+          >
+
+            <input
+              ref={node => {
+                input = node;
+              }}
+            />
+
+            <button type="submit">Update</button>
+
+          </form>
+        </div>
+      )}
+    </Mutation>
+  )
+}
+
+const App = () => {
+  return (
+    <React.Fragment>
+
+      <CreateArticle />
+        
+      <Query query={GET_ARTICLES}>
+        {({loading, error, data}) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+
+          return data.allArticles.nodes.map(({id, name}) => (
+            <UpdateArticle key={id} id={id} name={name} />
+          )) 
+        }}
+      </Query>
+
+    </React.Fragment>
+  )
+}
+
+function AppOld() {
+  const updateCache = (cache, {data}) => {
+    const existingArticles = cache.readQuery({
+      query: GET_ARTICLES
+    });
+
+    const newArticle = data.createArticle.article;
+    cache.writeQuery({
+      query: GET_ARTICLES,
+      data: {
+        allArticles: {
+          nodes: [
+            ...existingArticles,
+            newArticle
+          ]
+        }
+      }
+    });
+  }
+
   const { data, loading, error } = useQuery(GET_ARTICLES);
+  const [createArticle] = useMutation(CREATE_ARTICLE, {update: updateCache});
+
+  let input;
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
@@ -25,7 +196,37 @@ function App() {
     <React.Fragment>
 
       <h1>Articles</h1>
-      
+
+      <form 
+        onSubmit={e => {
+          e.preventDefault();
+
+          if (input.value) {
+            createArticle({
+              variables: {
+                input: {
+                  article: {
+                    name: input.value
+                  }
+                }
+              }
+            });
+          }          
+
+          input.value = '';
+        }}
+      >
+
+        <input
+          ref={node => {
+            input = node;
+          }}
+        />
+
+        <button type="submit">Create</button>
+
+      </form>
+
       {data && data.allArticles && data.allArticles.nodes.map((article, index) => (
         <div key={index}>
           {article.name}
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
